66627ae0690dda2d46e645a3713eead4744c5dc9
liushuai0606 <liushuai0606@gmail.com>
2019-10-22 22:57:35 +0900

    commit2

--- re_ip.py
+++ re_ip.py
@@ -8,3 +8,18 @@ print('-'*80)
 print('%-7s : %s' % ('接口',  result[0]))
 print('%-7s : %s' % ('IP地址', result[1]))
 print('%-7s : %s' % ('状态', result[5]))
+
+#方法2
+import re
+
+str2 = 'Port-channel1.189     192.168.189.254  YES     CONFIG   UP  DOWN'
+
+result2 = re.match(r'(\w\S+\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+\w+\s+\w+\s+(UP|DOWN)\s+\w+',
+                   str2.strip()).groups()
+
+result_format = '{0:<10}:{1:<30}'
+
+print('-'*80)
+print(result_format.format('接口2', result2[0]))
+print(result_format.format('IP地址2', result2[1]))
+print(result_format.format('状态2', result2[2]))
