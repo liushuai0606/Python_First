3f5dc2b2c4be5650701c8b1c5ec6e94dd5021acd
liushuai0606 <liushuai0606@gmail.com>
2019-10-22 10:08:45 +0900

    commit3

--- QYTANG's day.py
+++ QYTANG's day.py
@@ -1,5 +1,11 @@
+#方法1
 department1 = 'QYTANG\'day'
 department2 = '2014-9-28'
 
-
 print(department1 + ' ' + department2)
+
+#方法2
+department3 = "QYTANG'day"
+department4 = "2014-9-28"
+
+print(department3 + ' ' + department4)
