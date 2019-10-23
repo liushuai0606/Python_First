242e8577c8e97c55ca9d697b5c54f801e63f9529
liushuai0606 <liushuai0606@gmail.com>
2019-10-24 00:05:58 +0900

    commit1

--- 3day_MAC.py
+++ 3day_MAC.py
@@ -5,7 +5,7 @@ str = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
 result = re.match(r'(\d{1,4})\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w\S+\d)',
                   str.strip()).groups()
 
-result_format = '{0:<10}:{1:<30}'
+result_format = '{0:<10}: {1:<30}'
 
 print('-'*80)
 print(result_format.format('VLAN ID', result[0]))
@@ -15,9 +15,11 @@ print(result_format.format('Interface', result[3]))
 
 str1 = 'TCP server 172.16.1.101:443 localserver 172.16.66.1:53710, idle 0:01:09, bytes 27575949, flags UIO'
 
-result1 = re.match(r'(\w+)\s+server\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})\s+localserver\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}),\s+idle\s+(\d{1,2}):(\d{1,2}):(\d{1,2}), bytes\s+(\d+),\s+flags\s+(\w+)',str1.strip()).groups()
+result1 = re.match(r'(\w+)\s+server\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})\s+localserver\s+'
+                   r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}),\s+idle\s+(\d{1,2}):(\d{1,2}):(\d{1,2}),'
+                   r' bytes\s+(\d+),\s+flags\s+(\w+)',str1.strip()).groups()
 
-result_format1 = '{0:<15}:{1:<30}'
+result_format1 = '{0:<15}: {1:<30}'
 
 print('-'*80)
 print(result_format1.format('protoclo', result1[0]))
---
+++ 4day.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+#ifconfig_result = os.popen('ifconfig ' + 'ens160').read()
+ifconfig_result = 'ens160: flags=4163<UP,BROADCAST,RUNNING,MULTICAST> mtu 1500' \
+                  'inet 172.16.66.166 netmask 255.255.255.0 broadcast 172.16.66.255' \
+                  ' inet6 fe80::250:56ff:feab:59bd prefixlen 64 scopeid 0x20<link>' \
+                  'ether 00:50:56:ab:59:bd txqueuelen 1000 (Ethernet)' \
+                 'RX packets 174598769 bytes 1795658527217 (1.6 TiB)' \
+                 'RX errors 1 dropped 24662 overruns 0 frame 0' \
+                 'TX packets 51706604 bytes 41788673420 (38.9 GiB)' \
+                 'TX errors 0 dropped 0 overruns 0 carrier 0 collisions 0'
+
+#正则表达式查找ip,掩码，广播和Mac地址
+ipv4_add = re.findall(r'inet\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[0]
+netmask = re.findall(r'netmask\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[0]
+broadcast = re.findall(r'broadcast\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[0]
+mac_addr = re.findall(r'ether\s+[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})',
+                      ifconfig_result)[0]
+
+#产生网关的IP地址
+ipv4_list = ipv4_add.split('.')
+ipv4_list[3] = '254'
+gateway = '.'.join(ipv4_list)
+
+#格式化字符串
+format_string = '{0:<15}: {1:<30}'
+
+#打印结果
+print('-'*80)
+print(format_string.format('ipv4_add', ipv4_add[0]))
+print(format_string.format())
+print(format_string.format())
+print(format_string.format())
