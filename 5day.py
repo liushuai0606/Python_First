740626b21b39984035186876c741b2757deb52d2
liushuai0606 <liushuai0606@gmail.com>
2019-10-25 10:35:40 +0900

    commit3

--- 5day.py
+++ 5day.py
@@ -1,4 +1,15 @@
 import os
+import re
+
 route_n_result = os.popen('ipconfig/all').read()
 
-print(route_n_result)
+for route in route_n_result.strip().split('\n')[2:]:
+    re_route = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
+                        r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+'
+                        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
+                        r'(\w+)\s+\d+\s+\d+\s+\d+ |w',
+                        route.strip()).group()
+
+    if re_route[1] == 'UG':
+        print('网关为：', re_route[0])
+        break
