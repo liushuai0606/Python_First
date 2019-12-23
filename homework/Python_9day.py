908f2d6b0203a895754cbba6c365a65a76d12cb9
liushuai0606 <liushuai0606@gmail.com>
2019-12-23 22:03:02 +0900

    commit1

---
+++ homework/Python_9day.py
@@ -0,0 +1,32 @@
+import paramiko
+import re
+
+
+def qytang_ssh(ip, username, password, port=22, cmd='show run'):
+    import paramiko
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, username=username, password=password, timeout=5, compress=True)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    return stdout.read().decode()
+
+def ssh_get_route(ip, username, password):
+    route_result = qytang_ssh(ip, username, password, cmd='route -n')
+    for route in route_result.split('\n')[2:-1]:
+        re_route = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
+                            r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+'
+                            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+'
+                            r'(\w+)\s+\d+\s+\d+\s+\d+ \w+',
+                            route.strip())
+
+        if re_route:
+            if re_route.groups()[1] == 'UG':
+                return re_route.groups()[0]
+            print(re_route)
+
+if __name__ == '__main__':
+    print(qytang_ssh('192.168.1.110', 'root', 'Kd2@@456'))
+    print(qytang_ssh('192.168.1.110', 'root', 'Kd2@@456', cmd='pwd'))
+    print('网关为：')
+    print(ssh_get_route('192.168.1.110', 'root', 'Kd2@@456'))
